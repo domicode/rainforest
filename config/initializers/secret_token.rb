5c946faf9d3d4e1636e4c44264e59493468aad0e
domicode <domi@domidesign.ch>
2014-04-14 15:18:35 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rainforest::Application.config.secret_key_base = '3e28d634a077621527582c765ca00b2858ecd2e747f57a6d28a255e5df6ab682ebef3d7e0a93bd9fbdd3672101edabe25f523b31bc71dfee86d2179426a53f89'
