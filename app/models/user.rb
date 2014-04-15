4bb6ebadea04942910a15e351b02f813e09a6ac2
domicode <domi@domidesign.ch>
2014-04-15 14:16:56 -0400

    added user names

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   has_many :products, :through => :reviews 
 
   validates :email, :presence => true, :uniqueness => {message: "already taken, please chose another one"}
+  validates :name, :presence => true
 
   has_secure_password
 end
