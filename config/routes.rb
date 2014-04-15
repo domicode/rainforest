cc8eee82435d54c4db21dfb98ee2c66cac4e686a
domicode <domi@domidesign.ch>
2014-04-14 20:19:09 -0400

    added users and login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rainforest::Application.routes.draw do
+  get "users/new"
+  get "users/create"
+  get "new/create"
   resources :products
+  resources :users, :only => [:new, :create]
   root to: "products#index"
   end
